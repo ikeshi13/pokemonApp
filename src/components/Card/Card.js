fcf8e06e9d286c78e95bf7a69d92f55a7d17ad5f
池田慎太郎 <ikeshi0015@icloud.com>
2024-12-01 11:03:29 +0900

    日本語訳に変更

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Card.css";
 
 const Card = ({ pokemon }) => {
+  const [pokemonName, setPokemonName] = useState([]);
+  const [pokemonImage, setPokemonImage] = useState([]);
+  const [pokemonType, setPokemonType] = useState([]);
+
+  const loadPokemonDetails = async (data) => {
+    // ポケモンの名前（日本語）を取得
+    let responseName = await fetch(data.species.url);
+    let result = await responseName.json();
+    let jaName = result.names.find((name) => name.language.name === "ja").name;
+    setPokemonName(jaName);
+
+    // ポケモンのタイプ（日本語）を取得
+    const responseType = await Promise.all(data.types.map((type) => fetch(type.type.url).then((res) => res.json())));
+    const typeNames = responseType.map((type) => type.names.find((name) => name.language.name === "ja").name);
+    const typeName = typeNames.join("/");
+    setPokemonType(typeName);
+  };
+
+  useEffect(() => {
+    loadPokemonDetails(pokemon);
+    setPokemonImage(pokemon.sprites.other.home.front_default);
+  }, []);
+
   return (
     <div className="card">
-      <div className="cardImg">
-        <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
-      </div>
-      <h3 className="cardName">{pokemon.name}</h3>
+      <img src={pokemonImage} className="cardImg"></img>
+      <h3 className="cardName">{pokemonName}</h3>
       <div className="cardTypes">
-        <div>タイプ</div>
-        {pokemon.types.map((type) => {
-          return (
-            <div key={type.type.name}>
-              <span className="typeName">{type.type.name}</span>
-            </div>
-          );
-        })}
+        <span className="typeName">タイプ：{pokemonType}</span>
       </div>
       <div className="cardInfo">
         <div className="cardData">
